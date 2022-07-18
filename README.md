e9827244d2ab2a030345655b58b7a81c69d31ecb
yenru0 <yenru0604@gmail.com>
2022-07-18 17:02:23 +0900

    something

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # burnt_math
 탄 김치찌개 같은 수학 문제 집합
+
+# ProblemTemplate
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import random
 
 import numpy as np
 import sympy as sp
-from sympy.abc import x, y, z
 import jinja2
 
-a = sp.symbols("a:100")
+sp.Rational
+
+x, y, z = sp.symbols("x, y, z")
+a_ = sp.symbols("a:100")
 
 
 def render_from_template(directory, template_name, **kwargs):
@@ -14,12 +16,41 @@ def render_from_template(directory, template_name, **kwargs):
     template = env.get_template(template_name)
     return template.render(**kwargs)
 
+
+# TEMPLATE CLASS
+
+class ExactProblem:
+    def __init__(self, exprs, solution, question):
+        self.exprs = exprs
+        self.solution = solution
+        self.question = question
+
+
 class ProblemTemplate:
-    def __init__(self, expr, parameter_count, parameter_range, tags: list = [], question_message = r"{0}의 값은?",):
-        self.expr = expr
-        self.tags = tags
+    def __init__(self, exprs: list,
+                parameter_count, parameter_range, #a[0:100]
+                solver  = lambda x: x[0].doit(),
+                question_maker = lambda x: f"\\(\displaystyle{sp.latex(x[0])}\\)",
+                solution_maker = lambda s: f"\\(\displaystyle{sp.latex(s)}\\)",
+                props: dict = {},
+                tags: list = [],):
+        if isinstance(exprs, list):
+            self.exprs = exprs
+        else:
+            self.exprs = [exprs]
+        self.solver = solver
         self.parameter_count = parameter_count
         self.parameter_range = parameter_range
+        self.question_maker = question_maker
+        self.solution_maker = solution_maker
+        self.props = props # properties
+
+        self.tags = tags
+
+    def make_exact(self, rng=True):
+        params = [(a_[i], random.choice(self.parameter_range[i])) for i in range(self.parameter_count)]
+        subs_exprs = [expr.subs(params) for expr in self.exprs]
+        return ExactProblem(subs_exprs, self.solution_maker(self.solver(subs_exprs)), self.question_maker(subs_exprs))
 
 class TemplateManager:
     def __init__(self,):
@@ -30,9 +61,7 @@ class TemplateManager:
 
     def generate_problem(self, name):
         tmp: ProblemTemplate = self.templates[name]
-        params = [(a[i], random.choice(tmp.parameter_range[i])) for i in range(tmp.parameter_count)]
-        n_exp = tmp.expr.subs(params)
-        return n_exp
+        return tmp.make_exact()
 
     def randomly_generate_problems(self, k: int):
         keys = random.choices(list(self.templates.keys()), k=k)
@@ -41,9 +70,27 @@ class TemplateManager:
 TMX = TemplateManager()
 
 TMX.register_template(
+    "단항분수확장적분",
+    ProblemTemplate(
+        sp.Integral(1/x**a_[0], (x, 1, a_[1])),
+        2, [[-sp.Rational(1, 2), sp.Rational(1, 2), -sp.Rational(1, 3), 1, 2, 3,], [2, 3, 4],]
+    )
+)
+
+TMX.register_template(
+    "로그적분-이차다항식치환적분",
+    ProblemTemplate(
+        sp.Integral((2*x+a_[0])/(x**2+a_[0]*x+a_[1]), (x, 1, 2)),
+        2, [[0, 1, 2, 3], [0, 1, 2, 3]]
+    )
+)
+
+
+
+TMX.register_template(
     "삼각적분-사인",
     ProblemTemplate(
-        sp.Integral(sp.sin(a[0]*x), (x, a[1]/a[0], a[2]/a[0])),
+        sp.Integral(sp.sin(a_[0]*x), (x, a_[1]/a_[0], a_[2]/a_[0])),
         3, [[1, 2, 3],
         [0, sp.pi/6, sp.pi/4, sp.pi/3, sp.pi/2, 2*sp.pi/3, 3*sp.pi/4, 5*sp.pi/6, sp.pi],
         [0, sp.pi/6, sp.pi/4, sp.pi/3, sp.pi/2, 2*sp.pi/3, 3*sp.pi/4, 5*sp.pi/6, sp.pi],
@@ -54,7 +101,7 @@ TMX.register_template(
 TMX.register_template(
     "삼각적분-코사인",
     ProblemTemplate(
-        sp.Integral(sp.cos(a[0]*x), (x, a[1]/a[0], a[2]/a[0])),
+        sp.Integral(sp.cos(a_[0]*x), (x, a_[1]/a_[0], a_[2]/a_[0])),
         3, [[1, 2, 3],
         [0, sp.pi/6, sp.pi/4, sp.pi/3, sp.pi/2, 2*sp.pi/3, 3*sp.pi/4, 5*sp.pi/6, sp.pi],
         [0, sp.pi/6, sp.pi/4, sp.pi/3, sp.pi/2, 2*sp.pi/3, 3*sp.pi/4, 5*sp.pi/6, sp.pi],
@@ -65,7 +112,7 @@ TMX.register_template(
 TMX.register_template(
     "삼각적분-탄젠트",
     ProblemTemplate(
-        sp.Integral(sp.tan(a[0]*x), (x, a[1]/a[0], a[2]/a[0])),
+        sp.Integral(sp.tan(a_[0]*x), (x, a_[1]/a_[0], a_[2]/a_[0])),
         3, [[1, 2, 3],
         [-sp.pi/3, -sp.pi/4, -sp.pi/6, 0 , sp.pi/6, sp.pi/4, sp.pi/3],
         [-sp.pi/3, -sp.pi/4, -sp.pi/6, 0 , sp.pi/6, sp.pi/4, sp.pi/3],
@@ -76,7 +123,7 @@ TMX.register_template(
 TMX.register_template(
     "삼각적분-코탄젠트(pos)",
     ProblemTemplate(
-        sp.Integral(sp.cot(a[0]*x), (x, a[1]/a[0], a[2]/a[0])),
+        sp.Integral(sp.cot(a_[0]*x), (x, a_[1]/a_[0], a_[2]/a_[0])),
         3, [[1, 2, 3],
         [sp.pi/6, sp.pi/4, sp.pi/3],
         [sp.pi/6, sp.pi/4, sp.pi/3],
@@ -87,7 +134,7 @@ TMX.register_template(
 TMX.register_template(
     "삼각적분-시컨트스퀘어",
     ProblemTemplate(
-        sp.Integral(sp.sec(a[0]*x)**2, (x, a[1]/a[0], a[2]/a[0])),
+        sp.Integral(sp.sec(a_[0]*x)**2, (x, a_[1]/a_[0], a_[2]/a_[0])),
         3, [[1, 2, 3],
         [-sp.pi/3, -sp.pi/4, -sp.pi/6, 0 , sp.pi/6, sp.pi/4, sp.pi/3],
         [-sp.pi/3, -sp.pi/4, -sp.pi/6, 0 , sp.pi/6, sp.pi/4, sp.pi/3],
@@ -98,22 +145,27 @@ TMX.register_template(
 TMX.register_template(
     "삼각적분-코시컨트스퀘어",
     ProblemTemplate(
-        sp.Integral(sp.csc(a[0]*x)**2, (x, a[1]/a[0], a[2]/a[0])),
+        sp.Integral(sp.csc(a_[0]*x)**2, (x, a_[1]/a_[0], a_[2]/a_[0])),
         3, [[1, 2, 3],
         [sp.pi/6, sp.pi/4, sp.pi/3, sp.pi/2, 2*sp.pi/3, 3*sp.pi/4, 5*sp.pi/6],
         [sp.pi/6, sp.pi/4, sp.pi/3, sp.pi/2, 2*sp.pi/3, 3*sp.pi/4, 5*sp.pi/6],
         ],
     )
 )
+
 TMX.register_template(
-    "삼각치환-"
+    "삼각치환-일반삼각",
+    ProblemTemplate(
+        sp.Integral((a_[0]*(a_[1]**2-x**2))**(sp.Rational(1, 2)), (x, 0, a_[1]* a_[2])),
+        3, [[1, 2, 3], [1, 2, 3], [sp.Rational(1, 2), sp.sqrt(2)/2, sp.sqrt(3)/2]]
+    )
 )
+
 TMX.register_template(
     "삼각치환-탄젠트",
     ProblemTemplate(
-    sp.Integral(a[0]/(x**2+a[1]**2)**(a[2]), (x, 0, a[1])),
-    3, [[1, 2, 3], [1, 2, 3,], [sp.Rational(1, 2), 1]],
-    tags= ["integral", "trigonometric substitution"]),
+    sp.Integral(a_[0]/(x**2+a_[1]**2)**(a_[2]), (x, 0, a_[1])),
+    3, [[1, 2, 3], [1, 2, 3,], [sp.Rational(1, 2), 1]],)
 )
 
 
@@ -121,26 +173,24 @@ class RandomProblemGenerator:
     def __init__(self, tm):
         self.template_manager: TemplateManager = tm
         self.problems = []
-        self.solved = []
+
     def generate(self, n):
-        self.problems = self.template_manager.randomly_generate_problems(n)
-        self.solved = [problem.doit() for problem in self.problems]
+        self.problems = self.template_manager.randomly_generate_problems(n) # exactProblems 
     
     def print(self):
         for i, problem in enumerate(self.problems):
-            print(i, problem, self.solved[i])
+            print(i, problem.question, problem.solution)
     
     def pprint(self):
         for i, problem in enumerate(self.problems):
             sp.pprint(i)
-            sp.pprint(problem)
-            sp.pprint(self.solved[i])
+            sp.pprint(problem.question)
+            sp.pprint(problem.solution)
 
     def make_document(self,):
-        html = "<ol>\n"
 
-        latex_probs = [f"\\(\displaystyle{sp.latex(prob, mode='plain')}\\)" for prob in self.problems]
-        latex_solved = [f"\\(\displaystyle{sp.latex(sol, mode='plain')}\\)" for sol in self.solved]
+        latex_probs = [prob.question for prob in self.problems]
+        latex_solved = [prob.solution for prob in self.problems]
         p = render_from_template(".", "html_paper_template.html", problems = latex_probs)
         s = render_from_template(".", "html_solution_template.html", solved = latex_solved)
 
@@ -151,7 +201,7 @@ class RandomProblemGenerator:
             f.write(s)
 
 if __name__ == '__main__':
-    sp.init_printing(use_latex='mathjax')
+    #sp.init_printing(use_latex='mathjax')
     rpg = RandomProblemGenerator(TMX)
     rpg.generate(50)
     rpg.make_document()
